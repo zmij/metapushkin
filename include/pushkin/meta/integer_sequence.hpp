5c89eafccf80af5b6b5b2c246dbdf152a85f281b
zmij <sergei.a.fedorov@gmail.com>
2017-03-09 13:51:25 +0300

    Utilities to build integral sequences

---
+++ include/pushkin/meta/integer_sequence.hpp
@@ -0,0 +1,116 @@
+/*
+ * integer_sequence.hpp
+ *
+ *  Created on: Mar 9, 2017
+ *      Author: zmij
+ */
+
+#ifndef PUSHKIN_UTIL_INTEGER_SEQUENCE_HPP_
+#define PUSHKIN_UTIL_INTEGER_SEQUENCE_HPP_
+
+#include <utility>
+
+namespace psst {
+namespace meta {
+
+/**
+ * Metafunction to join sequences
+ */
+template < typename ... T >
+struct join;
+
+template < typename T >
+struct join < T > {
+    using type = T;
+};
+
+#if __cplusplus >= 201402L
+
+template < typename T, T ... A, T... B >
+struct join< ::std::integer_sequence<T, A...>, ::std::integer_sequence<T, B...> > {
+    using type = ::std::integer_sequence<T, A..., B...>;
+};
+
+namespace detail {
+
+template < typename T, T Min, T Max, T ... Values >
+struct unwrap_range_impl {
+    static_assert(Min < Max, "Start of range must be less than ending");
+    using type = typename unwrap_range_impl<T, Min, Max - 1, Max, Values ... >::type;
+};
+
+template < typename T, T V, T ... Values >
+struct unwrap_range_impl<T, V, V, Values...> {
+    using type = ::std::integer_sequence< T, V, Values... >;
+};
+
+template < typename T, T Min, T Max, T ... Values >
+struct reverse_unwrap_range_impl {
+    static_assert(Min < Max, "Start of range must be less than ending");
+    using type = typename reverse_unwrap_range_impl<T, Min + 1, Max, Min, Values ... >::type;
+};
+
+template < typename T, T V, T ... Values >
+struct reverse_unwrap_range_impl<T, V, V, Values...> {
+    using type = ::std::integer_sequence< T, V, Values... >;
+};
+
+const ::std::size_t template_depth = 128;
+
+template < typename T, T Min, T Max >
+struct unwrap_range;
+template < typename T, T Min, T Max >
+struct reverse_unwrap_range;
+
+template < typename T, T Min, T Max >
+struct split_range_helper {
+    using type = typename join<
+            typename unwrap_range_impl<T, Min, Min + template_depth - 1>::type,
+            typename unwrap_range<T, Min + template_depth, Max>::type
+    >::type;
+};
+
+template < typename T, T Min, T Max >
+struct unwrap_range :
+    ::std::conditional< (Max - Min < template_depth),
+         unwrap_range_impl<T, Min, Max>,
+         split_range_helper<T, Min, Max>
+    >::type {};
+
+template < typename T, T Min, T Max >
+struct reverse_split_range_helper {
+    using type = typename join<
+            typename reverse_unwrap_range<T, Min + template_depth, Max>::type,
+            typename reverse_unwrap_range_impl<T, Min, Min + template_depth - 1>::type
+    >::type;
+};
+template < typename T, T Min, T Max >
+struct reverse_unwrap_range :
+    ::std::conditional< (Max - Min < template_depth),
+         reverse_unwrap_range_impl<T, Min, Max>,
+         reverse_split_range_helper<T, Min, Max>
+    >::type {};
+
+template < typename T, T Min, T Max, bool Reverse >
+struct make_integer_sequence_impl : unwrap_range<T, Min, Max> {};
+
+template < typename T, T Min, T Max >
+struct make_integer_sequence_impl<T, Min, Max, true> : reverse_unwrap_range<T, Max, Min> {};
+
+} /* namespace detail */
+
+
+/**
+ * Metafunction to make integer sequences in range [Min, Max]
+ * If Min > Max, the sequence will be reversed.
+ */
+template < typename T, T Min, T Max >
+struct make_integer_sequence :
+    detail::make_integer_sequence_impl<T, Min, Max, (Min > Max )> {};
+
+#endif /* __cplusplus >= 201402L */
+
+} /* namespace meta */
+} /* namespace psst */
+
+#endif /* PUSHKIN_UTIL_INTEGER_SEQUENCE_HPP_ */
